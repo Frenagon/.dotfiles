813d6d05cd677d2ae10233e5312bf40d9e5367c9
Francisco Aceves <v-fralopez@expediagroup.com>
2024-01-10 13:30:22 -0600

    exa: ignore .DS_Store files

--- .config/zsh/directories.zsh
+++ .config/zsh/directories.zsh
@@ -1,5 +1,5 @@
 # List directory contents
-alias lsa='exa -lah --icons --git --sort=ext --sort=name --group-directories-first'
+alias lsa='exa -lah --icons --git --sort=ext --sort=name --group-directories-first --ignore-glob=.DS_Store'
 alias l='lsa'
-alias ll='exa -lh --icons --git --sort=ext --sort=name --group-directories-first'
-alias la='exa -lAh --icons --git --sort=ext --sort=name --group-directories-first'
+alias ll='exa -lh --icons --git --sort=ext --sort=name --group-directories-first --ignore-glob=.DS_Store'
+alias la='exa -lAh --icons --git --sort=ext --sort=name --group-directories-first --ignore-glob=.DS_Store'
