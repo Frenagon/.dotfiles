a2be30a1f22939b7b5cf3a31c6b6a0b7f8028a17
Francisco Aceves <frenagon@gmail.com>
2024-06-16 21:29:59 -0600

    exa: remove --git option

--- .config/zsh/directories.zsh
+++ .config/zsh/directories.zsh
@@ -1,5 +1,5 @@
 # List directory contents
-alias lsa='exa -lah --icons --git --sort=ext --sort=name --group-directories-first'
+alias lsa='exa -lah --icons --sort=ext --sort=name --group-directories-first'
 alias l='lsa'
-alias ll='exa -lh --icons --git --sort=ext --sort=name --group-directories-first'
-alias la='exa -lAh --icons --git --sort=ext --sort=name --group-directories-first'
+alias ll='exa -lh --icons --sort=ext --sort=name --group-directories-first'
+alias la='exa -lAh --icons --sort=ext --sort=name --group-directories-first'
